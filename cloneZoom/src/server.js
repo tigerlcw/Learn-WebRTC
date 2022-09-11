8770dde801de93470720618b6b0510b97ddc5711
tigerlcw <temp2temp@daum.net>
2022-09-12 00:13:17 +0900

    websocket add

--- cloneZoom/src/server.js
+++ cloneZoom/src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import path from 'path';
+import WebSocket, { WebSocketServer } from 'ws';
+import http from 'http';
+
 const __dirname = path.resolve();
 
 const app = express();
@@ -8,10 +11,13 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/src/views');
 
 app.use('/public', express.static(__dirname + '/src/public'));
-app.get('/', (req, res) => res.render('home'));
-app.get('/*', (req, res) => res.render('/')); // 기본 url 설정
+app.get('/', (_, res) => res.render('home'));
+app.get('/*', (_, res) => res.render('/')); // 기본 url 설정
 
 const handleListening = () =>
   // listening 설정
   console.log(`Listening on: http://localhost:3000`);
-app.listen(3000, handleListening);
+
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server }); // http, ws server 동시 실행
+server.listen(3000, handleListening);
