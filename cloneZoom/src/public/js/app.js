1605173c34c4d3daaf605a9a607c2b7d6acea3ed
LINKER <temp2temp@daum.net>
2022-09-13 23:48:42 +0900

    socket connect event

--- cloneZoom/src/public/js/app.js
+++ cloneZoom/src/public/js/app.js
@@ -1,2 +1,18 @@
 //alert('test');
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener('open', () => {
+  console.log('Connected to Server ✅');
+});
+
+socket.addEventListener('message', (message) => {
+  console.log('just got this: ', message.data, 'from the server');
+});
+
+socket.addEventListener('close', () => {
+  console.log('Disconnected from Server ❌');
+});
+
+setTimeout(() => {
+  socket.send('hello from the browser!');
+}, 3000);
--- cloneZoom/src/server.js
+++ cloneZoom/src/server.js
@@ -23,7 +23,12 @@ const wss = new WebSocketServer({ server }); // http, ws server 동시 실행
 
 // ws 부분
 wss.on('connection', (socket) => {
-  console.log(socket);
+  console.log('Connected to Browser ✅');
+  socket.on('close', () => console.log('Disconnected from the Browser ❌'));
+  socket.on('message', (message) => {
+    console.log(message);
+  });
+  socket.send('hello!!!');
 });
 
 server.listen(3000, handleListening);
